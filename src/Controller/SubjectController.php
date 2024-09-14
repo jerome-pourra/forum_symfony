1e15344c3737a7909db62c2adc47cc541ff0f44a
Jérôme Pourra <j.pourra.perso@gmail.com>
2024-09-14 22:56:24 +0200

    feat: localhost:port/ = subjects list :)

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,17 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Subject;
 use App\Form\SubjectFiltersType;
 use App\Service\SubjectService;
-use App\Service\Ui\SubjectUiService;
-use App\Utils\RequestContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/subjects')]
+#[Route('')]
 class SubjectController extends AbstractController
 {
 
